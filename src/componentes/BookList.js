fd9f6b35a6d2198d6cdcfbd15171c9acaf3b4fbe
juancasanchez6 <jcsanchezr04@educarex.es>
2023-11-21 10:29:31 +0100

    Update-Book_App

--- src/componentes/BookList.js
+++ src/componentes/BookList.js
@@ -1,12 +1,53 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
+import { API_URL } from "../API";
+import axios from "axios";
+import { useAppContext } from "./context/appContext";
 
 const BookList = () => {
-  return (
-    <div className='book-list'>
-      BookList
-    </div>
-  )
-}
+  const [books, setBooks] = useState([]);
 
-export default BookList
+  const { favourites, addToFavourites, removeFromFavourites } = useAppContext();
+
+  console.log("Favoritos son: ", favourites);
+
+  const favouritesChecker = (id) => {
+    const boolean = favourites.some((book) => book.id === id);
+    return boolean;
+  };
+
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <div key={book.id} className="book">
+          <div>
+            <h4>{book.title}</h4>
+          </div>
+          <div>
+            <img src={book.image_url} alt="#" />
+          </div>
+          <div>
+            {favouritesChecker(book.id) ? (
+              <button onClick={() => removeFromFavourites(book)}>
+                Quitar Fav
+              </button>
+            ) : (
+              <button onClick={() => addToFavourites(book)}>Añadir Fav</button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookList;
